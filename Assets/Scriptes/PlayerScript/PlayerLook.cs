5bfbf2110bdd47d2b35e702cc14e8a65ba37bb21
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-11-11 22:07:02 +0100

    Character Control

---
+++ Assets/Scriptes/PlayerScript/PlayerLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseSens = mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X")*mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y")*mouseSens;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+       
+    }
+}
---
+++ Assets/Scriptes/PlayerScript/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player's Body")]
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -9.82f;
+
+    [Header("Player's Feet")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+
+    void Update()
+    {
+        // Checking the Subfloor
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Player Movment
+        float realSpeed = speed * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move.normalized * realSpeed);
+
+        //Player Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+       
+    }
+}
