3ec4ba0ab35855dfd07f3d0e9c09795f57173298
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-11-11 23:14:13 +0100

    Change the camera position when crouching and creating a player prefab

--- Assets/Scriptes/PlayerScript/PlayerLook.cs
+++ Assets/Scriptes/PlayerScript/PlayerLook.cs
@@ -7,6 +7,7 @@ public class PlayerLook : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 100f;
     [SerializeField] private Transform playerBody;
     private float xRotation = 0f;
+    private float yStartPosition = 1.18f;
 
     void Start()
     {
@@ -24,6 +25,15 @@ public class PlayerLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.localPosition = Vector3.zero ;
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0f, yStartPosition, 0f);
+        }
        
     }
 }
